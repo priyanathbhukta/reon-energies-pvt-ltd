909b22a1dde2fb3cacc53b8fb7061a9fabd4451c
Priyanath Bhukta <priyanathbhukta@gmail.com>
2026-03-26 15:46:17 +0530

    string connection

--- server/db.js
+++ server/db.js
@@ -4,16 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new pg.Pool({
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT) || 5432,
-  database: process.env.DB_NAME || 'reon_customer_info_db',
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || '123456789',
-});
-
-pool.on('error', (err) => {
-  console.error('Unexpected error on idle client', err);
-  process.exit(-1);
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
